d1162123c09ee4a830535dd0680857b52d8a96fd
MaryMihn <54401098+MaryMihn@users.noreply.github.com>
2020-04-26 16:11:08 +0300

    ready shop (#2)
    * made remove buttons
    
    * finish work

--- 1-basket/main.js
+++ 1-basket/main.js
@@ -3,31 +3,42 @@ class Basket {
         this.name = name;
     }
 
-    products = [];
+    products = {};
 
-    getProduct(index){
-        return this.products[index];
-    };
-
-    removeProduct(index){
-        this.products.splice(index, 1)
+    removeProduct(id){
+        delete this.products[id]
     };
 
     addProduct(product){
-        this.products.push(product);
+        const { id } = product;
+        if (!this.products[id]){
+            this.products[id] = { count: 0, product: product }
+        }
+        this.products[id].count++
     };
 
+    minusProduct(product){
+        const { id } = product;
+        if (this.products[id].count===1) {
+            this.removeProduct(id)
+        } else {
+            this.products[id].count--
+        }
+    }
+
     getProducts(){
-        return this.products
+        return Object.values(this.products)
     };
 
     getPrice (){
-        return this.products.reduce((sum, product) => sum + product.price, 0);
+        return this.getProducts().reduce((sum, product) => sum + product.product.price*product.count, 0);
     };
 }
 
 class Product {
-    constructor({ name, price, picture }){
+
+    constructor({ id, name, price, picture }){
+        this.id = id;
         this.name = name;
         this.price = price;
         this.picture = picture;
@@ -35,40 +46,95 @@ class Product {
 }
 
 const products = [
-    new Product({ name: 'Lemon', price: 700, picture: 'picture/lemon.jpg' }),
-    new Product({ name: 'Kumquat', price: 800, picture: 'picture/kumquat.jpg' }),
-    new Product({ name: 'Mandarin', price: 695, picture: 'picture/mandarin.jpg' }),
-    new Product({ name: 'Lime', price: 588, picture: 'picture/lime.jpg' }),
-    new Product({ name: 'Limquat', price: 945, picture: 'picture/limquat.jpg' }),
-    new Product({ name: 'Grapefruit', price: 1020, picture: 'picture/grapefruit.jpg' }),
+    new Product({ id: 1, name: 'Lemon', price: 700, picture: 'picture/lemon.jpg' }),
+    new Product({ id: 2, name: 'Kumquat', price: 800, picture: 'picture/kumquat.jpg' }),
+    new Product({ id: 3, name: 'Mandarin', price: 695, picture: 'picture/mandarin.jpg' }),
+    new Product({ id: 4, name: 'Lime', price: 588, picture: 'picture/lime.jpg' }),
+    new Product({ id: 5, name: 'Limquat', price: 945, picture: 'picture/limquat.jpg' }),
+    new Product({ id: 6, name: 'Grapefruit', price: 1020, picture: 'picture/grapefruit.jpg' }),
 ];
+
+function getProduct(id){
+    return products.find((product)=> product.id===id)
+}
 
 let basket = new Basket("main");
 
 let htmlProducts = products
-  .map(({ name, picture, price }, index) => `<div class="Product">
+  .map(( { id, name, picture, price }) => `<div class="Product">
     <div class="name">${name}</div>
     <div class="img"><img class="img" src="${picture}" alt="pic"></div>
     <div class="bottom">
       <div class="price">${price}</div>
-      <button class="buy" data-id="${index}">Buy</button>
+      <button class="buy" data-id="${id}">Buy</button>
     </div>
   </div>`)
-  .join('')
+  .join('');
 
 document.getElementById("Products").innerHTML = htmlProducts;
 
-Array.from(document.getElementsByClassName('buy')).forEach(element => {
-    element.addEventListener('click', ({ target }) => {
-        const id = +target.dataset.id
-        const product = products[id]
-        basket.addProduct(product)
+function renderChoice(basket) {
+    const chosenProducts = basket.getProducts();
 
-        const chosenProducts = basket.getProducts()
+    if (chosenProducts.length === 0) {
+        document.getElementById('choice').innerHTML = '';
+        document.getElementById('price').innerHTML = '';
+        return
+    }
 
-        if (chosenProducts.length) {
-            const htmlChoice = `Products: ${chosenProducts.map(({ name }) => name).join(', ')}. Price: ${basket.getPrice()}.`
-            document.getElementById('choice').innerHTML = htmlChoice;
-        }
+    const htmlChoice = `<p class="pro">Products:</p><br> ${chosenProducts
+        .map(({count, product: {id, name, picture, price }}) => `<div class="choosen"> 
+        <img class="imgMini" src="${picture}" alt="pic">
+        <div class="miniName"><p class="pri">Your plant - </p> ${name} 
+           <div class="miniPrice"><p class="pri">Price : </p> ${price}</div>
+           <div class="miniPrice"><p class="pri">Count : </p> ${count}</div>
+        </div>
+      </div> <button class="remove" data-id="${id}">Remove</button>
+             <button class="button plus" data-id="${id}">+</button>
+             <button class="button minus" data-id="${id}">-</button>`)
+        .join('')}`;
+    const htmlPrice = `<p class="pri">Total Price:</p> ${basket.getPrice()}.`;
+    document.getElementById('choice').innerHTML = htmlChoice;
+    document.getElementById('price').innerHTML = htmlPrice;
+
+
+    Array.from(document.getElementsByClassName('plus')).forEach(element => {
+        element.addEventListener('click', ({target}) => {
+            const id = +target.dataset.id;
+            const product = getProduct(id)
+            basket.addProduct(product)
+            renderChoice(basket)
+        })
     })
-})
+
+    Array.from(document.getElementsByClassName('minus')).forEach(element => {
+        element.addEventListener('click', ({target}) => {
+            const id = +target.dataset.id;
+            const product = getProduct(id)
+            basket.minusProduct(product)
+            renderChoice(basket)
+        })
+    })
+
+    Array.from(document.getElementsByClassName('remove')).forEach(element => {
+        element.addEventListener('click', ({target}) => {
+            const id = +target.dataset.id;
+            basket.removeProduct(id)
+            renderChoice(basket)
+        })
+    })
+}
+
+Array.from(document.getElementsByClassName('buy')).forEach(element => {
+    element.addEventListener('click', ({target}) => {
+        const id = +target.dataset.id;
+        const product = getProduct(id)
+        basket.addProduct(product);
+
+        renderChoice(basket)
+    })
+});
+
+
+
+
